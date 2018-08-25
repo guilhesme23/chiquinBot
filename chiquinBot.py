7af24f65656f1c57bdd2d1bf9c51ec22d949db13
Guilherme Marques <marquesguilherme3@gmail.com>
2018-08-25 19:35:48 -0300

    Add flake8

--- chiquinBot.py
+++ chiquinBot.py
@@ -10,14 +10,16 @@ inMusicConversation = False
 token = os.environ['BOT_API_TOKEN']
 bot = telebot.TeleBot(token)
 
+
 # Intents
 def search_intent(message):
-	content = message.text
-	content = content.lower()
-	if ('musica' in content) and ('procura' in content):
-		return True
-	else:
-		return False
+    content = message.text
+    content = content.lower()
+    if ('musica' in content) and ('procura' in content):
+        return True
+    else:
+        return False
+
 
 # Handlers
 # Hello
@@ -25,45 +27,59 @@ def search_intent(message):
 def send_welcome(message):
     bot.reply_to(message, u"Fala meu consagrado!")
 
+
 # Help
 @bot.message_handler(commands=['help'])
 def bot_info(message):
-	bot.send_message(chat_id=message.chat.id, text='''Fala meu confederado!
-Eu me chamo Little Chico, e sou um bot que vai te ajudar a buscar musicas no Spotify!!
+    bot.send_message(
+        chat_id=message.chat.id,
+        text='''Fala meu confederado!
+Eu me chamo Little Chico,
+e sou um bot que vai te ajudar a buscar musicas no Spotify!!
 Pra pesquisar, só me pede pra procurar uma musica ai ;)''')
+
 
 # Handling song research
 @bot.message_handler(func=search_intent)
 def receive_message(message):
-		bot.send_message(chat_id=message.chat.id, text="é pra já")
-		markup = types.ForceReply(selective=False)
-		msg = bot.reply_to(message, u"Qual o nome da musica que você gostaria?", reply_markup=markup)
-		bot.register_next_step_handler(msg, get_desired_song)
+        bot.send_message(chat_id=message.chat.id, text="é pra já")
+        markup = types.ForceReply(selective=False)
+        msg = bot.reply_to(
+            message,
+            u"Qual o nome da musica que você gostaria?",
+            reply_markup=markup)
+        bot.register_next_step_handler(msg, get_desired_song)
+
 
 def get_desired_song(message):
-		id = message.chat.id
-		bot.send_photo(chat_id=id, photo=open('./img/img2.jpg', 'rb'))
-		bot.send_chat_action(id, 'typing')
-		search = search_song(message.text)
-		time.sleep(2)
-		if search:
-			bot.send_message(chat_id=id, text=('Musica: ' + search[0]))
-			bot.send_message(chat_id=id, text=search[1])
-		else:
-			bot.send_message(chat_id=id, text='Foi mal man, achei essa parada aí não :/')
+        id = message.chat.id
+        bot.send_photo(chat_id=id, photo=open('./img/img2.jpg', 'rb'))
+        bot.send_chat_action(id, 'typing')
+        search = search_song(message.text)
+        time.sleep(2)
+        if search:
+            bot.send_message(chat_id=id, text=('Musica: ' + search[0]))
+            bot.send_message(chat_id=id, text=search[1])
+        else:
+            bot.send_message(
+                chat_id=id,
+                text='Foi mal man, achei essa parada aí não :/')
+
 
 # Handling 'I dont uderstand'
 @bot.message_handler(func=lambda message: True)
 def sorry(message):
-	select = random.random() * 100
-	id = message.chat.id
-	if select < 33.3:
-		bot.send_message(chat_id=id,
-		                 text='Qual é men, fala direito. Entendi foi nada kkkk')
-	elif (select >= 33.3) and (select < 80):
-		bot.send_message(chat_id=id,
-		                 text='Foi mal consagrado, consegui entender não :(')
-	else:
-		bot.send_photo(chat_id=id, photo=open('./img/img1.jpg', 'rb'))
+    select = random.random() * 100
+    id = message.chat.id
+    if select < 33.3:
+        bot.send_message(
+            chat_id=id,
+            text='Qual é men, fala direito. Entendi foi nada kkkk')
+    elif (select >= 33.3) and (select < 80):
+        bot.send_message(chat_id=id,
+                         text='Foi mal consagrado, consegui entender não :(')
+    else:
+        bot.send_photo(chat_id=id, photo=open('./img/img1.jpg', 'rb'))
+
 
 bot.polling()
--- helpers.py
+++ helpers.py
@@ -3,35 +3,42 @@ import os
 import base64
 from pprint import pprint
 
-# Exemplo de request
-r = requests.get('https://jsonplaceholder.typicode.com/posts').json()
 
 def parse_query(query=str):
-  query = query.lower().replace(' ', '+')
-  return query
+    query = query.lower().replace(' ', '+')
+    return query
+
 
 def get_auth():
-  client_id = os.environ['CLIENT_ID']
-  client_secret = os.environ['CLIENT_SECRET']
-  secret = client_id + ':' + client_secret
-  auth = base64.b64encode(secret.encode('utf-8'))
-  headers = {'Authorization': 'Basic ' + auth.decode('utf-8')}
-  auth = requests.post('https://accounts.spotify.com/api/token',data={'grant_type': 'client_credentials'},headers=headers).json()
+    client_id = os.environ['CLIENT_ID']
+    client_secret = os.environ['CLIENT_SECRET']
+    secret = client_id + ':' + client_secret
+    auth = base64.b64encode(secret.encode('utf-8'))
+    headers = {'Authorization': 'Basic ' + auth.decode('utf-8')}
+    auth = requests.post(
+        'https://accounts.spotify.com/api/token',
+        data={'grant_type': 'client_credentials'},
+        headers=headers).json()
 
-  return auth['access_token']
+    return auth['access_token']
+
 
 def search_song(query):
-  query = parse_query(query)
-  auth = get_auth()
-  uri = "https://api.spotify.com/v1/search"
-  header = {'Authorization': 'Bearer ' + auth}
+    query = parse_query(query)
+    auth = get_auth()
+    uri = "https://api.spotify.com/v1/search"
+    header = {'Authorization': 'Bearer ' + auth}
 
-  response = requests.get(uri, {'q':query, 'type':'track', 'limit': 1}, headers=header).json()
-  pprint(response)
+    response = requests.get(
+        uri,
+        {'q': query, 'type': 'track', 'limit': 1},
+        headers=header).json()
+    pprint(response)
 
-  if not 'error' in response and response['tracks']['items']:
-    result = (response['tracks']['items'][0]['name'],
-              response['tracks']['items'][0]['external_urls']['spotify'])
-    return result
-  else:
-    return ()
+    if 'error' not in response and response['tracks']['items']:
+        result = (
+            response['tracks']['items'][0]['name'],
+            response['tracks']['items'][0]['external_urls']['spotify'])
+        return result
+    else:
+        return ()
